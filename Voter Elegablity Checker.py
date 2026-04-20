08959e31a4ffe8899663c9c4cae8ef9a86136d88
malkeet-codes <singhmalkeet0869@gmail.com>
2026-04-21 01:28:12 +0530

    Add files via upload
    A simple program that checks whether a user is eligible to vote based on age criteria.

---
+++ Voter Elegablity Checker.py
@@ -0,0 +1,8 @@
+#Voter Elegablity Checker
+
+vage=18                              
+age=int(input("what  is your age"))
+if age>=vage:
+   print("you are eligible to vote")
+else:
+   print("Sorry,you are not eligible to vote")
