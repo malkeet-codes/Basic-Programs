9153fd32a3668bebcfafa585e91dabdaedc98d87
malkeet-codes <singhmalkeet0869@gmail.com>
2026-04-21 01:25:36 +0530

    Add files via upload
    A simple program that evaluates student performance based on input grades and provides corresponding feedback.

---
+++ Grade Evaluation System.py
@@ -0,0 +1,17 @@
+
+#Grade Evaluation System
+
+grade=input("What is your grade?")
+print(grade)
+if grade=="A" or grade=="a":
+   print("Excellent")
+elif grade=="B" or grade=="b":
+   print("Good")
+elif grade=="C" or grade=="c":
+   print("Well Done")
+elif grade=="D" or grade=="d":
+   print("Average")
+elif grade=="E" or grade=="e":
+   print("Work Hard")
+else:
+   print("FAIL")
